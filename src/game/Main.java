82708ba1940991835068117a813a51ad61771b89
Wallison Vieira de Aquino <wall.aquinoo@gmail.com>
2025-01-16 16:01:33 +0000

    att

---
+++ src/game/Main.java
@@ -0,0 +1,12 @@
+package game;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.setupPlayers();
+        game.listPlayers(false);
+        // Adicione aqui a lógica para iniciar o jogo
+    }
+}
+
+    // cd /workspaces/JogoDeTabuleiro/src++++++++++ javac game/Main.java+++++++++++++ java game.Main
